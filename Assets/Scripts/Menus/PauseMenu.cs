f3221c3b1743e11aa2a66b4c89d1e328c39044af
Nicholas Lee <nklee2@uci.edu>
2024-03-09 23:37:56 -0800

    Sync host and client player animation and pause menu

--- Assets/Scripts/Menus/PauseMenu.cs
+++ Assets/Scripts/Menus/PauseMenu.cs
@@ -2,24 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Unity.Netcode;
+using System;
 
 // Contributors: Jacky, Adrian
-public class PauseMenu : Buttons
+public class PauseMenu : NetworkBehaviour
 {
     [SerializeField] private GameObject pauseMenu;
-    public static bool GameIsPaused;
-
-
+    public static bool isLocalGamePaused;
+    private NetworkVariable<bool> isGamePaused = new NetworkVariable<bool>(false);
+    private Dictionary<ulong, bool> playerPausedDictionary;
+    public event EventHandler OnMultiplayerGamePaused;
+    public event EventHandler OnMultiplayerGameUnpaused;
+    public static PauseMenu Instance { get; private set; }
     private void Start()
     {
-        GameIsPaused = false;
+        isLocalGamePaused = false;
     }
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-            if (GameIsPaused)
+            if (isLocalGamePaused)
             {
                 Resume();
             }
@@ -29,18 +34,69 @@ public class PauseMenu : Buttons
             }
         }
     }
+    private void Awake()
+    {
+        playerPausedDictionary = new Dictionary<ulong, bool>();
+    }
+    public override void OnNetworkSpawn()
+    {
+        isGamePaused.OnValueChanged += IsGamePaused_OnValueChanged;
+    }
+
+    private void IsGamePaused_OnValueChanged(bool previousValue, bool newValue)
+    {
+        if (isGamePaused.Value)
+        {
+            Time.timeScale = 0f;
+            OnMultiplayerGamePaused?.Invoke(this, EventArgs.Empty);
+        }
+        else
+        {
+            Time.timeScale = 1f;
+            OnMultiplayerGameUnpaused?.Invoke(this, EventArgs.Empty);
+        }
+    }
 
     public void Resume()
     {
         pauseMenu.SetActive(false);
-        Time.timeScale = 1f;
-        GameIsPaused = false;
+        UnpauseGameServerRpc();
+        //Time.timeScale = 1f;
+        isLocalGamePaused = false;
     }
 
     void Pause()
     {
         pauseMenu.SetActive(true);
+        PauseGameServerRpc();
         Time.timeScale = 0f;
-        GameIsPaused = true;
+        isLocalGamePaused = true;
+    }
+
+    [ServerRpc(RequireOwnership = false)]
+    private void PauseGameServerRpc(ServerRpcParams serverRpcParams = default)
+    {
+        playerPausedDictionary[serverRpcParams.Receive.SenderClientId] = true;
+        TestGamePausedState();
+    }
+    [ServerRpc(RequireOwnership = false)]
+    private void UnpauseGameServerRpc(ServerRpcParams serverRpcParams = default)
+    {
+        playerPausedDictionary[serverRpcParams.Receive.SenderClientId] = false;
+        TestGamePausedState();
+    }
+    private void TestGamePausedState()
+    {
+        foreach (ulong clientId in NetworkManager.Singleton.ConnectedClientsIds)
+        {
+            if (playerPausedDictionary.ContainsKey(clientId) && playerPausedDictionary[clientId])
+            {
+                //this player is paused
+                isGamePaused.Value = true;
+                return;
+            }
+        }
+        //all players are unpaused
+        isGamePaused.Value = false;
     }
 }
---
+++ Assets/Scripts/Menus/PauseMultiplayerUI.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMultiplayerUI : MonoBehaviour
+{
+    private void Start()
+    {
+        if (PauseMenu.Instance != null)
+        {
+            PauseMenu.Instance.OnMultiplayerGamePaused += PauseMenu_OnMultiplayerGamePaused;
+            PauseMenu.Instance.OnMultiplayerGameUnpaused += PauseMenu_OnMultiplayerGameUnpaused;
+            Hide();
+        }
+    }
+
+    private void PauseMenu_OnMultiplayerGameUnpaused(object sender, EventArgs e)
+    {
+        Hide();
+    }
+
+    private void PauseMenu_OnMultiplayerGamePaused(object sender, EventArgs e)
+    {
+        Show();
+    }
+
+    private void Show()
+    {
+        gameObject.SetActive(true);
+    }
+    private void Hide()
+    {
+        gameObject.SetActive(false);
+    }
+}
